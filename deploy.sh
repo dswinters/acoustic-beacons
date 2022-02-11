602167d140b59558f5dd57d805e601c695ee41a4
Dylan Winters <dylan.winters@oregonstate.edu>
2022-02-10 21:46:23 -0800

    use rsync for deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,10 @@
 #!/usr/bin/env bash
 
-mount_dir=$HOME/network-mounts
+local_dir=$(pwd)
 remote_dir="/home/pi/nav"
 
 for i in 0{1..2}; do
-
-  # Name should match an entry in .ssh/config
-  name="navbeacon$i"
-  echo "Deploying to $name"
-
-  # Copy code directory recursively
-  local=code
-  remote=pi@$name:/$remote_dir
-  scp -r $local $remote && echo "  - copied $local to $remote"
-
-  # Copy config file
-  local=config.yaml
-  remote=pi@$name:/$remote_dir/config.yaml
-  scp $local $remote && echo "  - copied $local to $remote"
-
-  local=beacons.service
-  remote=pi@$name:/$remote_dir/beacons.service
-  scp $local $remote && echo "  - copied $local to $remote"
-
-  local=beacons.conf
-  remote=pi@$name:/$remote_dir/beacons.conf
-  scp $local $remote && echo "  - copied $local to $remote"
-
-  local=install.sh
-  remote=pi@$name:/$remote_dir/install.sh
-  scp $local $remote && echo "  - copied $local to $remote"
-
+  host="navbeacon$i"
+  echo "Deploying to $host"
+  rsync -ra --partial-dir=/tmp --files-from=deploy_list.txt "$local_dir" "$host:$remote_dir"
 done
