3a036f07a441e422e3daa7886891122f311b9274
Dylan Winters <dylan.winters@oregonstate.edu>
2022-03-09 13:40:59 -0800

    re-organize system files, add udev rules

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-sudo cp beacons.service /etc/systemd/system/beacons.service
-sudo cp beacons.conf /etc/rsyslog.d/beacons.conf
+# systemd service
+sudo cp system/beacons.service /etc/systemd/system/beacons.service
+
+# rsyslog configuration
+sudo cp system/beacons.conf /etc/rsyslog.d/beacons.conf
+
+# udev rules for renaming serial ports
+sudo cp system/10-beacon.rules /etc/udev/rules.d/10-beacon.rules
+
+# enable & restart everything
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 sudo systemctl daemon-reload
 sudo systemctl restart rsyslog
 sudo systemctl enable beacons
